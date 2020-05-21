df9871c47f4822661cc81a8b08cf9d4e6235890f
vibgyor98 <www.sunny02@gmail.com>
2020-05-22 01:42:17 +0530

    chng About

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
@@ -6,6 +7,18 @@ const About = () => {
       <h1>About this app</h1>
       <p>React App to search Github Users</p>
       <p>Version: 1.0.0</p>
+      <p>
+        Developed by:
+        <Link to='https://www.linkedin.com/in/sourav-kar-78b093134/'>
+          <strong> Sourav Kar</strong>
+        </Link>
+      </p>
+      <p>
+        Check the repo, add more features:
+        <Link to='https://github.com/vibgyor98/gfinder'>
+          <strong> Github </strong>
+        </Link>
+      </p>
     </Fragment>
   );
 };
