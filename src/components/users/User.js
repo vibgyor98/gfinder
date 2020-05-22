5c0b11238a2864ccee496bbb8e9f613890de96a1
vibgyor98 <www.sunny02@gmail.com>
2020-05-22 17:12:44 +0530

    ++ mf 1

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -101,7 +101,7 @@ const User = ({ match: { params } }) => {
       <div className='card text-center'>
         <div className='badge badge-primary'>Followers: {followers}</div>
         <div className='badge badge-success'>Following: {following}</div>
-        <div className='badge badge-light'>Public Repos: {public_repos}</div>
+        <div className='badge badge-danger'>Public Repos: {public_repos}</div>
         <div className='badge badge-dark'>Public Gists: {public_gists}</div>
       </div>
       <Repos repos={repos} />
